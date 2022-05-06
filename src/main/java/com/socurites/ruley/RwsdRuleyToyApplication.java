4c9d69d2959e7b9cf6e885688d0d24b5c4756023
aaronsleepy <aaron@kmong.com>
2022-05-06 20:34:16 +0900

    first commit

---
+++ src/main/java/com/socurites/ruley/RwsdRuleyToyApplication.java
@@ -0,0 +1,13 @@
+package com.socurites.ruley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RwsdRuleyToyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RwsdRuleyToyApplication.class, args);
+	}
+
+}
