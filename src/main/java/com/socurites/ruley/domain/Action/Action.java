dce42e74db766af0994b698da89d6bc62fff8e30
aaronsleepy <aaron@kmong.com>
2022-05-07 18:34:17 +0900

    feature: Add Engine and Action

---
+++ src/main/java/com/socurites/ruley/domain/Action/Action.java
@@ -0,0 +1,6 @@
+package com.socurites.ruley.domain.Action;
+
+@FunctionalInterface
+public interface Action {
+    void execute();
+}
---
+++ src/main/java/com/socurites/ruley/main/BusinessRuleEngine.java
@@ -0,0 +1,28 @@
+package com.socurites.ruley.main;
+
+import com.socurites.ruley.domain.Action.Action;
+
+public class BusinessRuleEngine {
+    /**
+     * 액션 추가
+     * @param action
+     */
+    public void addAction(final Action action) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * 기본 리포트
+     * @return
+     */
+    public int count() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * 액션 실행
+     */
+    public void run() {
+        throw new UnsupportedOperationException();
+    }
+}
